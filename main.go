77901a4b7e07579453507481b0e5b09050dc92ce
Sembarang <agilfikriawan2803@gmail.com>
2025-05-18 21:30:30 +0800

    initial commit

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	app := fiber.New()
+	app.Get("/test", func(c *fiber.Ctx) error {
+		return c.JSON(fiber.Map{"message": "hello world"})
+	})
+	app.Listen(":8000")
+}
