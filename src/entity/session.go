e9a6020d0d6816c2d3392c04370c51e71e170efb
Sembarang <agilfikriawan2803@gmail.com>
2025-05-30 23:46:19 +0800

    add entity

---
+++ src/entity/session.go
@@ -0,0 +1,17 @@
+package entity
+
+type Session struct {
+	ID        string `gorm:"column:id;primaryKey;type:uuid;default:uuid_generate_v4()"`
+	UserID    string `gorm:"column:user_id;not null"`
+	token     string `gorm:"column:token;not null;uniqueIndex"`
+	issuedAt  string `gorm:"column:issued_at;not null;autoCreateTime;<-:create"`
+	ExpiresAt string `gorm:"column:expires_at;not null"`
+	revoked   bool   `gorm:"column:revoked;not null;default:false"`
+	CreatedAt string `gorm:"column:created_at;not null;autoCreateTime;<-:create"`
+	UpdatedAt string `gorm:"column:updated_at;not null;autoCreateTime;autoUpdateTime"`
+	User      User   `gorm:"foreignKey:user_id;references:id;OnDelete:CASCADE"`
+}
+
+func (s *Session) TableName() string {
+	return "sessions"
+}
---
+++ src/entity/user.go
@@ -0,0 +1,23 @@
+package entity
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type User struct {
+	ID        uuid.UUID `gorm:"column:id;primaryKey;type:uuid;default:uuid_generate_v4()"`
+	name      string    `gorm:"column:name;not null"`
+	Email     string    `gorm:"column:email;uniqueIndex;not null"`
+	Password  string    `gorm:"column:password;not null"`
+	Photo     *string   `gorm:"column:photo"`
+	createdAt time.Time `gorm:"column:created_at;not null;autoCreateTime;<-:create"`
+	UpdatedAt time.Time `gorm:"column:updated_at;not null;autoCreateTime;autoUpdateTime"`
+	Session   []Session `gorm:"foreignKey:user_id;references:id;OnDelete:CASCADE"`
+	Notes     []Notes   `gorm:"foreignKey:user_id;references:id;OnDelete:CASCADE"`
+}
+
+func (u *User) TableName() string {
+	return "users"
+}
