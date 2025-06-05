b0a4fd56ed75242a9c68b26015501e705167a923
Sembarang <agilfikriawan2803@gmail.com>
2025-06-05 11:44:13 +0800

    update di for user feature in app.go

--- src/api/app.go
+++ src/api/app.go
@@ -2,9 +2,8 @@ package api
 
 import (
 	"notes-management-api/src/api/auth"
-	"notes-management-api/src/api/auth/controllers"
-	"notes-management-api/src/api/auth/repository"
-	"notes-management-api/src/api/auth/services"
+	"notes-management-api/src/api/users"
+	"notes-management-api/src/shared/middleware"
 
 	"github.com/go-playground/validator/v10"
 	"github.com/gofiber/fiber/v2"
@@ -13,10 +12,15 @@ import (
 
 func App(app *fiber.App, db *gorm.DB, validate *validator.Validate) {
 	api := app.Group("/api/v1")
-	authRepositoryImpl := repository.NewAuthRepository(db)
-	authServiceImpl := services.NewAuthService(authRepositoryImpl, validate)
-	authControllerImpl := controllers.NewAuthController(authServiceImpl)
+	authRepositoryImpl := auth.NewAuthRepository(db)
+	authServiceImpl := auth.NewAuthService(authRepositoryImpl, validate)
+	authControllerImpl := auth.NewAuthController(authServiceImpl)
 	authRouter := auth.NewAuthRouter(authControllerImpl)
 	authRouter.AuthRoutes(api.Group("/auth"))
 
+	userRepositoryImpl := users.NewUserRepository(db)
+	userServiceImpl := users.NewUserService(userRepositoryImpl, validate)
+	userControllerImpl := users.NewUserController(userServiceImpl)
+	userRouter := users.NewUserRouter(userControllerImpl)
+	userRouter.UserRoutes(api.Group("/user", middleware.UserSession()))
 }
