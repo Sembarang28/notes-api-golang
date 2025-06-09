9d8223f17b9354502b2e3be8765c07bd36d5c666
Sembarang <agilfikriawan2803@gmail.com>
2025-06-09 14:49:43 +0800

    update migrations table category and user table relationship

--- migrations/20250526031256_create_tables.up.sql
+++ migrations/20250526031256_create_tables.up.sql
@@ -28,6 +28,7 @@ CREATE TABLE IF NOT EXISTS category (
   id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
   name VARCHAR(255) NOT NULL,
   description TEXT NOT NULL,
+  user_id UUID REFERENCES users(id) ON DELETE CASCADE,
   created_at TIMESTAMP DEFAULT NOW(),
   updated_at TIMESTAMP DEFAULT NOW()
 );
