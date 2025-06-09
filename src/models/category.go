a915517411fb6975a0b7b5f6c85502f46a6474dd
Sembarang <agilfikriawan2803@gmail.com>
2025-06-09 14:50:15 +0800

    update models for table category and user relationship

--- src/models/category.go
+++ src/models/category.go
@@ -5,9 +5,11 @@ import "time"
 type Category struct {
 	ID          string    `gorm:"column:id;primaryKey;type:uuid;default:uuid_generate_v4()"`
 	Name        string    `gorm:"column:name;not null;uniqueIndex"`
+	UserID      string    `gorm:"column:user_id;not null"`
 	Description string    `gorm:"column:description;not null"`
 	CreatedAt   time.Time `gorm:"column:created_at;not null;autoCreateTime;<-:create"`
 	UpdatedAt   time.Time `gorm:"column:updated_at;not null;autoCreateTime;autoUpdateTime"`
+	User        User      `gorm:"foreignKey:user_id;references:id;OnDelete:CASCADE"`
 	Notes       []Notes   `gorm:"foreignKey:category_id;references:id;OnDelete:CASCADE"`
 }
 
--- src/models/user.go
+++ src/models/user.go
@@ -5,15 +5,16 @@ import (
 )
 
 type User struct {
-	ID        string    `gorm:"column:id;primaryKey;type:uuid;default:uuid_generate_v4()"`
-	Name      string    `gorm:"column:name;not null"`
-	Email     string    `gorm:"column:email;uniqueIndex;not null"`
-	Password  string    `gorm:"column:password;not null"`
-	Photo     *string   `gorm:"column:photo;default:''"`
-	CreatedAt time.Time `gorm:"column:created_at;not null;autoCreateTime;<-:create"`
-	UpdatedAt time.Time `gorm:"column:updated_at;not null;autoCreateTime;autoUpdateTime"`
-	Session   []Session `gorm:"foreignKey:user_id;references:id;OnDelete:CASCADE"`
-	Notes     []Notes   `gorm:"foreignKey:user_id;references:id;OnDelete:CASCADE"`
+	ID        string     `gorm:"column:id;primaryKey;type:uuid;default:uuid_generate_v4()"`
+	Name      string     `gorm:"column:name;not null"`
+	Email     string     `gorm:"column:email;uniqueIndex;not null"`
+	Password  string     `gorm:"column:password;not null"`
+	Photo     *string    `gorm:"column:photo;default:''"`
+	CreatedAt time.Time  `gorm:"column:created_at;not null;autoCreateTime;<-:create"`
+	UpdatedAt time.Time  `gorm:"column:updated_at;not null;autoCreateTime;autoUpdateTime"`
+	Session   []Session  `gorm:"foreignKey:user_id;references:id;OnDelete:CASCADE"`
+	Notes     []Notes    `gorm:"foreignKey:user_id;references:id;OnDelete:CASCADE"`
+	Category  []Category `gorm:"foreignKey:user_id;references:id;OnDelete:CASCADE"`
 }
 
 func (u *User) TableName() string {
